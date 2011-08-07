e8c2eb8d0c6c986261744e43ecd9eb9a659d860c
Siddhant Sanyam <siddhant3s@gmail.com>
2011-08-07 17:48:12 +0530

    (split)Moved codebase/<everything> to root

--- codebase/scripts/uninstall.sh
+++
@@ -1,7 +0,0 @@
-CMSPATH=$1
-: ${CMSPATH:="."}
-echo "" > $CMSPATH/cms/config.inc.php
-rm $CMSPATH/.htaccess
-cp $CMSPATH/htaccess-dist $CMSPATH/.htaccess
-chmod 777 $CMSPATH/.htaccess
-echo "Pragyan CMS Uninstalled!"
